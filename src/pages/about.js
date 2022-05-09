67d03823fb4df718ed3cd3cd28916950aebb828d
javier <javi.spanish.dev@gmail.com>
2022-05-09 07:49:44 +0100

    tutorial part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const about = () => {
+  return (
+    <Layout pageTitle='About Page'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse quo laudantium harum quidem eaque impedit placeat aliquam amet alias tempora! Dolorem minima voluptates architecto nisi?</p>
+    </Layout>
+  );
+};
+
+export default about;
